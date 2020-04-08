d6111845e1ce966220c59b2e7a51386a4c0cce98
Cleber <cleber.silva@reitoria.ifpe.edu.br>
2020-04-07 21:52:16 -0300

    Alpha mirror exercise.

---
+++ ls02/alpha_mirror.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+int main (int argc, char *args[]) {
+    if (argc == 2) {
+        int i = 0;
+        while (args[1][i]) {
+            if (args[1][i] >= 'a' && args[1][i] <= 'z') {
+                args[1][i] = 'z' - args[1][i] + 'a';
+            } else if (args[1][i] >= 'A' && args[1][i] <= 'Z') {
+                args[1][i] = 'Z' - args[1][i] + 'A';
+            }
+            write(1, &args[1][i], 1);
+            i += 1;
+        }
+    }
+    write(1, "\n", 1);
+    return 0;
+}
